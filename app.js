38df18817b6448515bcc0899208c0eefda5e4081
Gideon Cameron <gideonwork20@gmail.com>
2025-06-11 00:02:09 -0700

    Working on the Hackerrank balanced Brackets solution

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 function isBalanced(s) {
     // Stack to keep track of opening brackets
+    const stack = [];
+
+    // Map to match closing brackets with their corresponding opening brackets
     
 
     
